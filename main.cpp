a25dc79263a568d360bbf02d46384faae07d21b0
Mark Romero <geraldo.romero@ucsp.edu.pe>
2020-11-05 11:53:43 -0500

    Correccion de nombre de la funcion

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
-void swap(char tabla[8][8], int x, int y, int a, int b){
-    int temp = tabla[x][y];
+void cambio_interno(char tabla[8][8], int x, int y, int a, int b){
+    char temp = tabla[x][y];
     tabla[x][y] = tabla[a][b];
     tabla[a][b] = temp;
 }
@@ -39,7 +39,7 @@ void cambio(char tabla[8][8], int x = 0, int y = 0, int a = 0, int b = 0){
     cin >> x >> y;
     cout << "Ingrese cambio" << endl;
     cin >> a >> b;
-    swap(tabla, x, y, a, b);
+    cambio_interno(tabla, x, y, a, b);
     tablero(tabla);
 }
 
