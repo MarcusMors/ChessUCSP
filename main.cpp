1a548f903f1ba30db2f934052826e5836f5e836d
Mark Romero <geraldo.romero@ucsp.edu.pe>
2020-11-07 10:37:05 -0500

    Sin clases

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
 #include "Librerias/Tablero/Tablero.h"
 using namespace std;
-
-class peon{
-    int cord_x=2;
-};
-
 int main(){
     char m[8][8] = {{'T', 'C', 'A', 'R', 'Q', 'A', 'C', 'T'},
                     {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
